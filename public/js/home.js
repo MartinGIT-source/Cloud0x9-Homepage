3b04ab0b72e2613923662f60a6aae492f3f9fd2b
Martin <martin.ostvik@appliedtechnology.se>
2020-06-12 13:54:44 +0200

    Added repository

---
+++ public/js/home.js
@@ -0,0 +1,51 @@
+$('a[href*="#"]')
+  .click(function(event) {
+      let target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          console.log('done');
+        });
+      }
+  });
+
+// set up text to print, each item in array is new line
+let aText = new Array(
+  "Our journey to the clouds",
+  );
+  let iSpeed = 100; // time delay of print out
+  let iIndex = 0; // start printing array at this posision
+  let iArrLength = aText[0].length; // the length of the text array
+  let iScrollAt = 20; // start scrolling up at this many lines
+   
+  let iTextPos = 0; // initialise text position
+  let sContents = ''; // initialise contents letiable
+  let iRow; // initialise current row
+   
+  function typewriter()
+  {
+   sContents =  ' ';
+   iRow = Math.max(0, iIndex-iScrollAt);
+   let destination = document.getElementById("typedtext");
+   
+   while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
+   }
+   destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+   if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+    if ( iIndex != aText.length ) {
+     iArrLength = aText[iIndex].length;
+     setTimeout("typewriter()", 500);
+    }
+   } else {
+    setTimeout("typewriter()", iSpeed);
+   }
+  }
+  
+  
+  typewriter();
